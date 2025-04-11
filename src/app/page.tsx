14785406364d92fd4b5f4bc4e9600733a34dd147
Mabaidul-work <mabaidul@appscrip.co>
2025-04-11 15:22:13 +0530

    title: lint stage issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <section className="flex items-center justify-center min-h-screen w-full">
       <div className="flex flex-col items-center justify-center w-full h-screen bg-gradient-to-b from-primary-500 to-secondary-500">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-white mb-5">
-          Nextjs Template A
+          Nextjs Template
         </h1>
         <p className="text-lg text-center text-white">
           A comprehensive Next.js project template with TypeScript, Tailwind
